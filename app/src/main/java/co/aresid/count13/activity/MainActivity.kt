657dfd98383285c3ad61e17198a582f91b32fe3f
René Spies <rene.j.spies@gmail.com>
2020-12-12 03:29:16 +0100

    Initialized the project and added all necessary dependencies. Updated the activity layout with CoordinatorLayout and a FAB. Added a custom Application class to initialize Timber logging.

--- app/src/main/java/co/aresid/count13/activity/MainActivity.kt
+++ app/src/main/java/co/aresid/count13/activity/MainActivity.kt
@@ -1,12 +1,20 @@
-package co.aresid.count13
+package co.aresid.count13.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.aresid.count13.R
+import timber.log.Timber
 
 class MainActivity: AppCompatActivity() {
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
+
+		Timber.d("onCreate: called")
+
 		super.onCreate(savedInstanceState)
+
 		setContentView(R.layout.activity_main)
+
 	}
+
 }
---
+++ app/src/main/java/co/aresid/count13/application/Count13Application.kt
@@ -0,0 +1,29 @@
+package co.aresid.count13.application
+
+import android.app.Application
+import timber.log.Timber
+
+
+/**
+ *    Created on: 12/12/2020
+ *    For Project: count13
+ *    Author: René Jörg Spies
+ *    Copyright: © 2020 ARES ID
+ */
+
+
+class Count13Application : Application() {
+
+    /*
+    ONLY EDIT IF NECESSARY!
+     */
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Initialize Timber
+        Timber.plant(Timber.DebugTree())
+
+    }
+
+}
