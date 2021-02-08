b57c7e5c07750b0e08b961394371fc65c7f3c9ad
René Jörg Spies <rene.j.spies@gmail.com>
2021-02-08 03:37:09 +0100

    Refactor: Greatly improved the BottomSheetNavigation by using a NavController
    I removed all the stuff I implemented before and made the navigation from the BottomSheet quite simple by leveraging Navigation Components NavController. NavController automates all the code I wrote before to make the navigation work. It's down to three lines of code now.

--- app/src/main/java/me/renespies/count13/Util.kt
+++ app/src/main/java/me/renespies/count13/Util.kt
@@ -1,9 +1,7 @@
 package me.renespies.count13
 
-import android.view.MenuItem
 import androidx.databinding.BindingAdapter
 import com.google.android.material.bottomappbar.BottomAppBar
-import com.google.android.material.navigation.NavigationView
 import timber.log.Timber
 
 
@@ -30,56 +28,6 @@ object Util {
         Timber.d("onNavigationItemSelected: called")
 
         setNavigationOnClickListener { block() }
-
-    }
-
-    /**
-     * BindingAdapter that shortcuts [NavigationView.setNavigationItemSelectedListener] by leveraging
-     * DataBinding. It can be used by XML expressions just like app:onClick on a button.
-     */
-    @BindingAdapter("app:onMenuItemSelected")
-    @JvmStatic
-    fun NavigationView.onMenuItemSelected(
-        block: MenuItemSelected
-    ) {
-
-        Timber.d("onMenuItemSelected: called")
-
-        setNavigationItemSelectedListener { block.onMenuItemSelected(it); false }
-
-    }
-
-    /**
-     * Short interface to use with [NavigationView.onMenuItemSelected] for
-     * XML lambdas with parameters.
-     */
-    interface MenuItemSelected {
-
-        fun onMenuItemSelected(menuItem: MenuItem)
-
-    }
-
-    enum class Navigate {
-
-        /**
-         * The default value to initialize any LiveData.
-         */
-        DEFAULT,
-
-        /**
-         * Value to signal the navigation to the CounterListFragment.
-         */
-        TO_HOME,
-
-        /**
-         * Value to signal the navigation to the SignUpFragment.
-         */
-        TO_SIGN_UP,
-
-        /**
-         * Value to signal the navigation to the SettingsFragment.
-         */
-        TO_SETTINGS
 
     }
 
--- app/src/main/java/me/renespies/count13/fragment/bottomnavigationsheet/BottomNavigationSheetFragment.kt
+++ app/src/main/java/me/renespies/count13/fragment/bottomnavigationsheet/BottomNavigationSheetFragment.kt
@@ -5,11 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import me.renespies.count13.R
-import me.renespies.count13.Util
 import me.renespies.count13.databinding.FragmentBottomNavigationSheetBinding
 import timber.log.Timber
 
@@ -40,43 +39,19 @@ class BottomNavigationSheetFragment : BottomSheetDialogFragment() {
         bottomNavigationSheetViewModel = ViewModelProvider(this).get(BottomNavigationSheetViewModel::class.java)
         binding.viewModel = bottomNavigationSheetViewModel
 
-        bottomNavigationSheetViewModel.navigateTo.observe(viewLifecycleOwner) {
-
-            navigateTo(it) // Navigate to the appropriate fragment
-
-        }
+        createBottomSheetNavigation()
 
         return binding.root
 
     }
 
-    /**
-     * Evaluates the [destination] parameter and navigates accordingly using
-     * a [androidx.navigation.NavController].
-     */
-    private fun navigateTo(destination: Util.Navigate) {
+    private fun createBottomSheetNavigation() {
 
-        Timber.d("navigateTo: called")
+        Timber.d("createBottomSheetNavigation: called")
 
         val navigationHostFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.navigation_host) as NavHostFragment // Find the NavigationHostFragment
         val navigationController = navigationHostFragment.navController // Collect the NavigationController from the NavigationHostFragment
-
-        // Build a NavigationOptions object with animation and a popUpTo target
-        val navigationOptions = NavOptions.Builder().setPopUpTo(R.id.counterListFragment, false).setEnterAnim(R.anim.fade_in).setPopEnterAnim(R.anim.fade_in).setExitAnim(R.anim.fade_out).setPopExitAnim(R.anim.fade_out).build()
-
-        // Use the NavigationController to navigate to the desired location
-        when (destination) {
-
-            Util.Navigate.DEFAULT -> {
-            } // Default branch is here for null safety
-
-            Util.Navigate.TO_HOME -> navigationController.navigate(R.id.counterListFragment, null, navigationOptions) // Navigate to CounterListFragment
-
-            Util.Navigate.TO_SIGN_UP -> navigationController.navigate(R.id.signUpFragment, null, navigationOptions) // Navigate to SignUpFragment
-
-            Util.Navigate.TO_SETTINGS -> navigationController.navigate(R.id.settingsFragment, null, navigationOptions) // Navigate to SettingsFragment
-
-        }
+        binding.navigationView.setupWithNavController(navigationController)
 
     }
 
