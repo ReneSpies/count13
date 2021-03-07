f1364a1d571e2ab6702fc85f36e9ede6b8f6129d
René Jörg Spies <rene.j.spies@gmail.com>
2021-03-07 08:45:26 +0100

    Feature: The counter note's description is displayed correctly
    The counter note's description is now displayed as single line only and shows three dots at the end if the text is long than the TextView's size.

--- app/src/main/java/me/renespies/count13/activity/MainActivity.kt
+++ app/src/main/java/me/renespies/count13/activity/MainActivity.kt
@@ -1,9 +1,7 @@
 package me.renespies.count13.activity
 
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import me.renespies.count13.databinding.ActivityMainBinding
@@ -16,7 +14,6 @@ class MainActivity: AppCompatActivity() {
 	private lateinit var binding: ActivityMainBinding // Binding for the layout
 	private lateinit var mainActivityViewModel: MainActivityViewModel // Corresponding ViewModel
 	
-	@RequiresApi(Build.VERSION_CODES.M)
 	override fun onCreate(savedInstanceState: Bundle?) {
 		
 		Timber.d("onCreate: called")
